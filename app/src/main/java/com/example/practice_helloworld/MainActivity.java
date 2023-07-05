17bb1407be48085d76580d1d855bbf483de7334d
ZhaoMeap <bill56523@gmail.com>
2023-07-06 03:28:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/practice_helloworld/MainActivity.java
@@ -0,0 +1,7 @@
+package com.example.practice_helloworld;
+
+public class MainActivity extends android.app.Activity{
+    public static void Main(String[] args){
+        System.out.println("helloWorld.");
+    }
+}
